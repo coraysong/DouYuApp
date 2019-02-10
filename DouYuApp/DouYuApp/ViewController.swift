11b605c3ed230cdb19f21b9a8335bd0ef06b4f68
coraysong <coraysong1991@msn.cn>
2019-02-10 21:11:15 +0800

    项目创建，加油！

---
+++ DouYuApp/DouYuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DouYuApp
+//
+//  Created by 宋超 on 2019/2/10.
+//  Copyright © 2019 宋超. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DouYuApp/DouYuAppTests/DouYuAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  DouYuAppTests.swift
+//  DouYuAppTests
+//
+//  Created by 宋超 on 2019/2/10.
+//  Copyright © 2019 宋超. All rights reserved.
+//
+
+import XCTest
+@testable import DouYuApp
+
+class DouYuAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
